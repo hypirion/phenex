0316e7625d91b2b883bb2ac8935354e0011b48de
Jean Niklas L'orange <jeannikl@hypirion.com>
2012-11-11 15:03:20 +0100

    Added utility fns and macros (->>).

--- phenex.lisp
+++ phenex.lisp
@@ -4,11 +4,25 @@
 
 (defconstant EPSILON 1e-9)
 
+(defun append-1 (x y)
+  "Adds the element y to the end of x."
+  (append x (list y)))
+
+(defmacro ->> (x &rest forms)
+ "Threads the expr through the forms. Inserts x as the last item in the first
+form. If there are more forms, inserts the first form as the last item in second
+form, etc."
+ (do ((up-next forms (cdr up-next))
+      (res x (append-1 (car up-next) res)))
+     ((null up-next) res)))
+
 (defmacro bind (&body body)
   `(multiple-value-bind ,@body))
 
 (defun sum (&rest elts)
   (apply #'reduce #'+ elts))
+
+;;;; ADABOOST
 
 (defun normalize (w-arr)
   "Destructively normalizes an array."
