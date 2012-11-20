5360892a9dc7983da91463f6454a844027aac511
Jean Niklas L'orange <jeannikl@hypirion.com>
2012-11-20 22:45:03 +0100

    Add -> as macro.

--- utils.lisp
+++ utils.lisp
@@ -8,6 +8,16 @@
   "Adds the element y to the end of x."
   (append x (list y)))
 
+(defmacro -> (x &rest forms)
+  "Threads the expr through the forms. Inserts x as the
+  second item in the first form, making a list of it if it is not a
+  list already. If there are more forms, inserts the first form as the
+  second item in second form, etc."
+  (if (null forms)
+      x
+      (destructuring-bind ((ff . fr) . r) forms
+	`(-> ,(list* ff x fr) ,@r))))
+
 (defmacro ->> (x &rest forms)
  "Threads the expr through the forms. Inserts x as the last item in the first
 form. If there are more forms, inserts the first form as the last item in second
