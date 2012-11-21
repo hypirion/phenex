dc1619665aec5b5d1dff9e26a0a28349d304cfb8
Jean Niklas L'orange <jeannikl@hypirion.com>
2012-11-21 02:26:13 +0100

    More util fns.

--- utils.lisp
+++ utils.lisp
@@ -9,10 +9,9 @@
   (append x (list y)))
 
 (defmacro -> (x &rest forms)
-  "Threads the expr through the forms. Inserts x as the
-  second item in the first form, making a list of it if it is not a
-  list already. If there are more forms, inserts the first form as the
-  second item in second form, etc."
+  "Threads the expr through the forms. Inserts x as the second item in the first
+  form. If there are more forms, inserts the first form as the second item in
+  second form, etc."
   (if (null forms)
       x
       (destructuring-bind ((ff . fr) . r) forms
@@ -25,6 +24,26 @@ form, etc."
  (do ((up-next forms (cdr up-next))
       (res x (append-1 (car up-next) res)))
      ((null up-next) res)))
+
+(defun update-in (ht ks f &rest args)
+  (destructuring-bind (k . r) ks
+    (if (null r)
+	(setf (gethash k ht) (apply f (gethash k ht) args))
+	(let ((v (gethash k ht)))
+	  (if (null v)
+	      (let ((nht (make-hash-table :test #'equal)))
+		(setf (gethash k ht) nht
+		      v nht)))
+	  (apply #'update-in v r f args)))))
+
+(defun get-in (ht ks)
+  (destructuring-bind (k . r) ks
+    (if (null r)
+	(gethash k ht)
+	(get-in (gethash k ht) r))))
+
+(defun fnil (f v)
+  (lambda (x &rest ys) (apply f (if (null x) v x) ys)))
 
 (defmacro bind (&body body)
   `(multiple-value-bind ,@body))
